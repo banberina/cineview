8b26883b4d2e641545f8054c6dd021b6d813be2b
banberina <berina.bandic@gmail.com>
2020-05-29 14:41:26 +0200

    Basic backend setup added.

---
+++ frontend/build/precache-manifest.55245052cef70f6954b61c4826c8b5e1.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4e987caab0d52ff1aee29289a4b6a99",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f24ac0884148ca7094f4",
+    "url": "/static/css/main.ad457878.chunk.css"
+  },
+  {
+    "revision": "46d562eee61531ce594e",
+    "url": "/static/js/2.5ac086d6.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.5ac086d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f24ac0884148ca7094f4",
+    "url": "/static/js/main.98e7e3fd.chunk.js"
+  },
+  {
+    "revision": "700d31d1c6f37b7bdc51",
+    "url": "/static/js/runtime-main.84578d5e.js"
+  },
+  {
+    "revision": "0fa3ad5560196424085f127d1a3dd26f",
+    "url": "/static/media/banner.0fa3ad55.jpg"
+  },
+  {
+    "revision": "b81dabc0bc6ca52fce921e3a928d5e0f",
+    "url": "/static/media/dots.b81dabc0.png"
+  },
+  {
+    "revision": "03ef1918e505c3e3471f9369ef7a638f",
+    "url": "/static/media/nucleo.03ef1918.eot"
+  },
+  {
+    "revision": "5987dd12fea78ce5f97ae601b08ec03c",
+    "url": "/static/media/nucleo.5987dd12.woff2"
+  },
+  {
+    "revision": "b17a118e13e53558658b681a0ebdad82",
+    "url": "/static/media/nucleo.b17a118e.ttf"
+  },
+  {
+    "revision": "f0b489a5dbbff08833d21024f9fcbd4e",
+    "url": "/static/media/nucleo.f0b489a5.woff"
+  },
+  {
+    "revision": "ca25707397fcc982d7dc1f82555f09a0",
+    "url": "/static/media/square1.ca257073.png"
+  },
+  {
+    "revision": "6619b9792d7d108bb589b51bbd560278",
+    "url": "/static/media/square2.6619b979.png"
+  },
+  {
+    "revision": "23fa76237f0066f4ffd8f20e77449887",
+    "url": "/static/media/square3.23fa7623.png"
+  },
+  {
+    "revision": "8fdf9ee20db40e5d1cf778bf99486cb7",
+    "url": "/static/media/square4.8fdf9ee2.png"
+  },
+  {
+    "revision": "ff004a0e5de54ba9dbd8583fa7e1b3f5",
+    "url": "/static/media/square5.ff004a0e.png"
+  }
+]);
