5fed1ddb7432e3960f366476e07cc504fbe6a92e
banberina <berina.bandic@gmail.com>
2020-05-29 14:52:43 +0200

    Basic backend setup added.

--- backend/index.js
+++ backend/index.js
@@ -3,15 +3,15 @@ const path = require('path');
 
 const app = express();
 
-// Serve the static files from the React app
-app.use(express.static(path.join(__dirname, '../frontend/build')));
-
-
-// Handles any requests that don't match the ones above
-app.get('*', (req,res) =>{
-    res.sendFile(path.join(__dirname+'../frontend/build/index.html'));
-});
-
+app.use('/',express.static('./../frontend/build'));
+app.get('/*',function (req,res) {
+    res.sendFile(path.join(__dirname,'./../frontend/build/index.html'), function(err) {
+        if (err) {
+          res.status(500).send(err)
+        }
+      })
+    })
+    
 const port = process.env.PORT || 8080;
 app.listen(port);
 
